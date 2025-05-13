e0a831105124398e623f3fa83016fdcf152c24f1
giulibrumatti <giulibrumatti97@gmail.com>
2025-05-13 14:24:49 -0300

    controller: add search for id

--- src/main/java/com/rh/empleadito/controller/EmployeeController.java
+++ src/main/java/com/rh/empleadito/controller/EmployeeController.java
@@ -1,17 +1,19 @@
 package com.rh.empleadito.controller;
 
+import com.rh.empleadito.exception.ResourceNotFoundException;
 import com.rh.empleadito.model.Employee;
 import com.rh.empleadito.service.IEmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("empleadito-app")
-@CrossOrigin(value = "http://localhost:5173")
+@CrossOrigin(value = "http://localhost:3000")
 public class EmployeeController {
     private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
@@ -29,5 +31,14 @@ public class EmployeeController {
     public Employee crearDuenio(@RequestBody Employee emp){
         logger.info("Empleado a agregar: " + emp);
         return empServ.addEmployee(emp);
+    }
+
+    @GetMapping("/buscarEmpleado/{id}")
+    public ResponseEntity<Employee> searchForId(@PathVariable Long id){
+        Employee emp = empServ.searchForEmployeeForId(id);
+        if(emp == null)
+            throw new ResourceNotFoundException("No se encontre el ID: "+ id);
+
+        return ResponseEntity.ok(emp);
     }
 }
